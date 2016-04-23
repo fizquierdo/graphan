9ff1873bd335c7f6eebeaeeef3ff480601c7967f
Fernando Izquierdo <fer.izquierdo@gmail.com>
2016-04-23 20:21:29 +0100

    added views to show words and add new words

--- webapp.rb
+++ webapp.rb
@@ -19,16 +19,38 @@ class GraphanServer
 		graph = @neo.execute_query(cypher)
 		graph["data"].map{|d| d[0]["data"]}
 	end
+
+	def add_word(word)
+		node = @neo.create_node(hanzi: word[:hanzi], 
+													 eng:		word[:eng], 
+													 pinyin:word[:pinyin])
+		@neo.add_label(node, "Word")
+	end
 end
+
 
 
 # Sinatra API 
 graphan = GraphanServer.new
 
 get '/' do 
-	"Graphan words: #{graphan.words} "
+	@words = graphan.words
+	erb :index
 end
 
 get '/people' do 
 	"Graphan members: #{graphan.people} members"
+end
+
+get '/addword' do 
+	erb :addword_form
+end
+
+# When a new word is submitted, store it in Graphene DB
+post '/addword' do 
+	new_word = {hanzi: params["hanzi"],
+							pinyin: params["pinyin"],
+							eng: params["eng"]}
+	graphan.add_word new_word
+	redirect '/'
 end
